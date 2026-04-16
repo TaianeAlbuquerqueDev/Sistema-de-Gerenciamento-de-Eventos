1ca06a2833fdb118630528915617f35e2ac40363
TaianeAlbuquerqueDev <taiane.albuquerque.1994@gmail.com>
2026-04-16 14:21:50 -0300

    Configs iniciais e html Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { EventList } from './pages/event-list/event-list';
+import { Dashboard } from './pages/admin/dashboard/dashboard';
+import { EventForm } from './pages/admin/event-form/event-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "home", pathMatch: 'full'},
+    {path: "login", component: Login},
+    {path: "home", component: EventList},
+    {
+        // Rotas admin
+        path: "admin", children: [
+            {path: "dashboard", component: Dashboard},
+            {path: "event-form", component: EventForm}
+        ]
+    }
+];
