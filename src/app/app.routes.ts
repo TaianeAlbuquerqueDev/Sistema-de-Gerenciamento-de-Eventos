efdfbc6a6e65c543142706b8c96f80c9982e2ba9
TaianeAlbuquerqueDev <taiane.albuquerque.1994@gmail.com>
2026-04-27 16:39:11 -0300

    FINAL

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,3 @@
-// import { Routes } from '@angular/router';
-// import { Dashboard } from './pages/admin/dashboard/dashboard';
-// import { EventForm } from './pages/admin/event-form/event-form';
-// import { Login } from './pages/login/login';
-// import { authGuard } from './guards/auth-guard';
-// import { EventList } from './pages/event-list/event-list';
-// import { EventDetail } from './pages/event-detail/event-detail';
-
-// export const routes: Routes = [
-//     { path: "", redirectTo: "home", pathMatch: 'full' },
-//     { path: "login", component: Login },
-//     { path: "home", component: EventList },
-//     {path: 'events/:id', component: EventDetail},
-
-//     {
-//         path: "admin",
-//         canActivate: [authGuard],
-//         children: [
-//             { path: "dashboard", component: Dashboard },
-//             { path: "events", component: EventForm }
-//         ]
-//     }
-// ];
 
 import { Routes } from '@angular/router';
 import { Dashboard } from './pages/admin/dashboard/dashboard';
@@ -38,7 +15,7 @@ export const routes: Routes = [
 
   {
     path: 'admin',
-    component: Dashboard,        // ← navbar do admin fica aqui
+    component: Dashboard,   
     canActivate: [authGuard],
     children: [
       { path: '', redirectTo: 'events', pathMatch: 'full' },
